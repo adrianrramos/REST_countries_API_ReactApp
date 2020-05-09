079b1a77e3eecf9a5b91dd56ce4270f269cb1b8c
Adrian Ramos <ramosadrian56@gmail.com>
2020-05-09 15:46:37 -0700

    add card styling

--- src/components/index/Card.js
+++ src/components/index/Card.js
@@ -15,39 +15,37 @@ const Card = ({
                 className="card-image"
                 src={flag} 
                 alt={name}
-                style={{
-                    width: '10vw',
-                    height: 'auto'
-                }}
             />
-            <Link to={`/nation/${name}`} className="card-link">
-                <h1 className="card-title">
-                    {name}
-                </h1>
-            </Link>
-            <div className="card-content-row">
-                <h4 className="card-content-label">
-                    Population: 
-                </h4>
-                <p className="card-content-text">
-                    {population}
-                </p>
-            </div>
-            <div className="card-content-row">
-                <h4 className="card-content-label">
-                    Region:
-                </h4>
-                <p className="card-content-text">
-                    {region}
-                </p>
-            </div>
-            <div className="card-content-row">
-                <h4 className="card-content-label">
-                    Capital: 
-                </h4>
-                <p className="card-content-text">
-                    {capital || 'Not Found'}
-                </p>
+            <div className="card-content-container">
+                <Link to={`/nation/${name}`} className="card-link">
+                    <h1 className="card-title">
+                        {name}
+                    </h1>
+                </Link>
+                <div className="card-content-row">
+                    <h4 className="card-content-label">
+                        Population: 
+                    </h4>
+                    <p className="card-content-text">
+                        {population}
+                    </p>
+                </div>
+                <div className="card-content-row">
+                    <h4 className="card-content-label">
+                        Region:
+                    </h4>
+                    <p className="card-content-text">
+                        {region}
+                    </p>
+                </div>
+                <div className="card-content-row">
+                    <h4 className="card-content-label">
+                        Capital: 
+                    </h4>
+                    <p className="card-content-text">
+                        {capital || 'Not Found'}
+                    </p>
+                </div>
             </div>
         </div>
     );
